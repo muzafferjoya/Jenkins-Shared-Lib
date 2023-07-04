d577091825955c9f999828e11423b90d4f84b96c
Muzaffar khan <muzaffar_k@tekditechnologies.com>
2023-07-04 15:27:59 +0530

    update

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,6 +1,8 @@
 def call(String project, String ImageTag, String hubUser){
     sh """
-    trivy image --format template --template "@/usr/local/share/trivy/templates/html.tpl" -o report.html ${hubUser}/${project}:latest > scan.txt
+    trivy image ${hubUser}/${project}:latest > scan.txt
     cat scan.txt
     """
 }
+
+// --format template --template "@/usr/local/share/trivy/templates/html.tpl" -o report.html
