9c970c840ab8375bd5fa461bb15695590a38c6e2
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2023-09-03 13:23:14 +0530

    Create publishDependencyCheckReport.groovy

---
+++ vars/publishDependencyCheckReport.groovy
@@ -0,0 +1,21 @@
+// File: vars/publishDependencyCheckReport.groovy
+
+def call(String reportFileName) {
+    post {
+        always {
+            // Archive the Dependency-Check HTML report as an artifact
+            archiveArtifacts artifacts: "reports/${reportFileName}", allowEmptyArchive: true
+
+            // Publish the Dependency-Check report to Jenkins
+            publishHTML(target: [
+                allowMissing: false,
+                alwaysLinkToLastBuild: true,
+                keepAll: true,
+                reportDir: 'reports',
+                reportFiles: "${reportFileName}",
+                reportName: 'Dependency-Check Report',
+                reportTitles: 'Dependency-Check Report'
+            ])
+        }
+    }
+}
