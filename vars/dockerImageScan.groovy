475eb7d3674fe969580990910ac0448e1eb85fcf
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2023-09-06 16:58:19 +0530

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,6 +1,6 @@
 def call(String project, String ImageTag, String hubUser){
     sh """
-    trivy image --format template --template "@/usr/local/share/trivy/templates/html.tpl" -o report.html ${hubUser}/${project}:latest
+    trivy image --format template --template "@/usr/local/share/trivy/templates/html.tpl" -o report.html ${hubUser}/${project}:latest > scan.txt
     // trivy image ${hubUser}/${project}:latest > scan.txt
     // cat scan.txt
     """
