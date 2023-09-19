e0f4ad8c146a151a2efd8a469e81ef0dfbe414fe
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2023-09-19 12:39:25 +0530

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -2,14 +2,7 @@ def call(String project, String ImageTag, String hubUser){
     sh """
     trivy --no-progress image --format template --template "@/usr/local/share/trivy/templates/html.tpl" -o report.html ${hubUser}/${project}:latest
     """
-                    publishHTML target : [
-                    allowMissing: true,
-                    alwaysLinkToLastBuild: true,
-                    keepAll: true,
-                    reportFiles: 'report.html',
-                    reportName: 'Trivy Scan',
-                    reportTitles: 'Trivy Scan'
-                ]
+
 }
 
 // There are 3 types of report output format of trivy ( Table , JSON, Template). I used html template for output report of trivy scan
